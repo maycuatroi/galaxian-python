863bfa282207215c7526cb31dc08333a2eed463e
Nguyen Anh Binh <sometimesocrazy@gmail.com>
2020-04-03 13:52:56 +0700

    sync code

--- corona_war_zone.py
+++ corona_war_zone.py
@@ -59,20 +59,21 @@ class Space:
                                  random.randint(star[1], star[2]),
                                  star[0])
 
-class AsteroidField:
-    def __init__(self, destination, max_asteroids = 10):
+class WarZone:
+    def __init__(self, destination, max_viruses = 10):
         self.__layout = destination
-        self.__max_asteroids = max_asteroids
+        self.__max_viruses = max_viruses
 
-        self.asteroid = ImageRegistry().get_images('data/asteroid_1_')
-        self.__nasteroids = len(self.asteroid)
-        self.__maxx = (self.__layout.get_rect().width - 
-                       self.asteroid[0].get_rect().width)
+        self.virus = ImageRegistry().get_images('data/asteroid_1_')
+        self.bacterias  = game.common.load_images('data/bacterias_1_')
+        self.__nvirus = len(self.virus)
+        self.__maxx = (self.__layout.get_rect().width -
+                       self.virus[0].get_rect().width)
         self.__maxy = self.__layout.get_rect().height
 
-        self.__asteroids = []
+        self.__viruses = []
 
-    def __rotate(self, surface, angle):
+    def __walkdown(self, surface, angle):
         original_rect = surface.get_rect()
         rotated_image = pygame.transform.rotate(surface, angle)
         rotated_rect = rotated_image.get_rect()
@@ -83,36 +84,106 @@ class AsteroidField:
         return rotated_image.subsurface(clipped_rect)
 
     def create_asteroid(self, x = None, vy = None):
-        if len(self.__asteroids) >= self.__max_asteroids:
+        if len(self.__viruses) >= self.__max_viruses:
             return
         if x is None:
             x = random.randint(0, self.__maxx)
         if vy is None:
             vy = random.randint(1, 3)
-        self.__asteroids.append((random.randint(0, self.__nasteroids - 1),
-                                 x, -100, vy))
+        self.__viruses.append((random.randint(0, self.__nvirus - 1),
+                               x, -100, vy,1))
 
-    def update(self, shots = None):
+    def update(self, shots = None,player=None):
         destroy = []
-        for a in range(len(self.__asteroids)):
-            ast = self.__asteroids[a]
-            self.__layout.blit(self.asteroid[ast[0]], (ast[1], ast[2]))
-            self.__asteroids[a] = ((ast[0] + 1) % self.__nasteroids,
-                                   ast[1], ast[2] + ast[3], ast[3])
+        for a in range(len(self.__viruses)):
+            ast = self.__viruses[a]
+            if ast[-1] ==1 :
+                virus = self.virus
+            else:
+                virus = self.bacterias
+            self.__layout.blit(virus[ast[0]], (ast[1], ast[2]))
+            self.__viruses[a] = ((ast[0] + 1) % self.__nvirus,
+                                 ast[1], ast[2] + ast[3], ast[3])
+            if ast[2] > self.__maxy + 10:
+                destroy.append(a)
+
+            if  shots is not None:
+                ar = pygame.Rect(ast[1], ast[2],
+                                 virus[ast[0]].get_rect().width,
+                                 virus[ast[0]].get_rect().height)
+                collision = ar.collidelist(shots.areas())
+                if (collision != -1):
+                    destroy.append(a)
+                    shots.destroy_shot(collision)
+
+            if player is not None:
+                ar = pygame.Rect(ast[1], ast[2],
+                                 virus[ast[0]].get_rect().width,
+                                 virus[ast[0]].get_rect().height)
+                collision = ar.collidelist(player.areas())
+
+
+        for a in destroy:
+            del(self.__viruses[a])
+
+
+class Corona:
+    def __init__(self, destination, max_viruses=10):
+        self.virus = ImageRegistry().get_images('data/asteroid_1_')
+        self.bacterias = game.common.load_images('data/bacterias_1_')
+        self.__nvirus = len(self.virus)
+        self.__maxx = (self.__layout.get_rect().width -
+                       self.virus[0].get_rect().width)
+        self.__maxy = self.__layout.get_rect().height
+
+        self.__viruses = []
+
+    def __walkdown(self, surface, angle):
+        original_rect = surface.get_rect()
+        rotated_image = pygame.transform.rotate(surface, angle)
+        rotated_rect = rotated_image.get_rect()
+        clipped_rect = pygame.Rect(
+            (rotated_rect.width - original_rect.width) / 2,
+            (rotated_rect.height - original_rect.height) / 2,
+            original_rect.width, original_rect.height)
+        return rotated_image.subsurface(clipped_rect)
+
+    def create_asteroid(self, x=None, vy=None):
+        if len(self.__viruses) >= self.__max_viruses:
+            return
+        if x is None:
+            x = random.randint(0, self.__maxx)
+        if vy is None:
+            vy = random.randint(1, 3)
+        self.__viruses.append((random.randint(0, self.__nvirus - 1),
+                               x, -100, vy, 1))
+
+    def update(self, shots=None):
+        destroy = []
+        for a in range(len(self.__viruses)):
+            ast = self.__viruses[a]
+            if ast[-1] == 1:
+                virus = self.virus
+            else:
+                virus = self.bacterias
+            self.__layout.blit(virus[ast[0]], (ast[1], ast[2]))
+            self.__viruses[a] = ((ast[0] + 1) % self.__nvirus,
+                                 ast[1], ast[2] + ast[3], ast[3])
             if ast[2] > self.__maxy + 10:
                 destroy.append(a)
 
             if not (shots is None):
                 ar = pygame.Rect(ast[1], ast[2],
-                                 self.asteroid[ast[0]].get_rect().width,
-                                 self.asteroid[ast[0]].get_rect().height)
+                                 virus[ast[0]].get_rect().width,
+                                 virus[ast[0]].get_rect().height)
                 collision = ar.collidelist(shots.areas())
                 if (collision != -1):
                     destroy.append(a)
                     shots.destroy_shot(collision)
 
         for a in destroy:
-            del(self.__asteroids[a])
+            del (self.__viruses[a])
+
 
 class PlayerShoot:
     def __init__(self, layout, max_shots = 1):
@@ -168,7 +239,7 @@ class Game:
         self.pshots = PlayerShoot(self.__playground, 2)
         self.player = game.player.Ship(self.__playground,         
                                        self.pshots.create)
-        self.asteroids = AsteroidField(self.__playground)
+        self.viruses = WarZone(self.__playground)
 
     def run(self):
 
@@ -177,7 +248,7 @@ class Game:
             self.__clock.tick(GAME_FPS)
 
             if random.randint(1, 20) == 3:
-                self.asteroids.create_asteroid()
+                self.viruses.create_asteroid()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -204,7 +275,7 @@ class Game:
             # Draw everything
             self.space.update()
             self.pshots.update()
-            self.asteroids.update(self.pshots)
+            self.viruses.update(self.pshots)
             self.player.update()
 
             self.__scr.blit(self.__playground, (150, 0))
@@ -213,7 +284,7 @@ class Game:
 if __name__ == '__main__':
     pygame.init()
     screen = game.common.Screen(windowed=('-win' in sys.argv),
-                                caption='MyGalaxian')
+                                caption='Corona wars zone')
 
     # Load all images
     ImageRegistry().load_image('data/background.png')
